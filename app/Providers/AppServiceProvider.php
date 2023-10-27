3f89d2dde0275a9b21dea256f7fa94feca702276
besufikad17 <besufikadtilahun1994@gmail.com>
2023-10-27 14:45:41 +0300

    docker and ngnix conf added

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Routing\UrlGenrator;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -19,6 +20,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+       if (env('APP_ENV') == 'production') {
+            $url->forceScheme('https');
+       }
     }
 }
