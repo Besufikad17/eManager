154c3d80772f9c6cae43b74a5500f418fe884dbe
besufikad17 <besufikadtilahun1994@gmail.com>
2023-10-27 15:29:07 +0300

    deploy script updated

--- scripts/00-laravel-deploy.sh
+++ scripts/00-laravel-deploy.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 echo "Running composer"
 composer global require hirak/prestissimo
+
+composer update --no-scripts
 composer install --no-dev --working-dir=/var/www/html
 
 echo "Caching config..."
